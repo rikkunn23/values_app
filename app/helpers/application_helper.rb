a85783c8aea87e288a861bcad9f4097332b63f11
rikkunn23 <riku1123suta@i.softbank.jp>
2021-11-20 15:45:02 +0900

    title finish

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+    def full_title(page_title = '')
+        base_title = "valuse"
+        if page_title.empty? #何も入っていなかった場合(provide(:title, ""))がなかった場合
+            base_title
+        else
+            page_title +"|"+base_title
+        end
+    end
+
 end

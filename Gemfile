d9403c76fa2ad34e13bee6ea59f2a4327bde5f6c
rikkunn23 <riku1123suta@i.softbank.jp>
2021-11-22 12:36:05 +0900

    usersmodel

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
 
+#パスワードのハッシュ化
+gem 'bcrypt',         '3.1.13'
 
 gem 'bootstrap-sass', '3.4.1'
 gem 'puma', '~> 5.0'
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord #ActiveRecordが適用
+
+    before_save { self.email = self.email.downcase}
+    #中身があるかどうか 長さなど ハッシュは()省略
+    validates :name,  presence: true, length: { maximum: 50 }
+    EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    # 一意性とメールアドレスの大文字・小文字の区別を無くす
+    uniqueness: { case_sensitive: false }
+
+    #パスワード
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
+end
